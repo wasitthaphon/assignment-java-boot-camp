ffbc36164f2c64245f02a1403a98811f93b62154
wasitthaphon <wasitthaphon@gmail.com>
2022-02-20 22:46:28 +0700

    feat: คลาส ResponseMessage ใช้กับ message ที่เป็น error handle #18
    เพิ่มคลาสใช้รอรับ error handler message เพื่อ response message

---
+++ wasitthaphon/src/main/java/com/example/wasitthaphon/models/ResponseMessage.java
@@ -0,0 +1,16 @@
+package com.example.wasitthaphon.models;
+
+public class ResponseMessage {
+    private String message;
+
+    public ResponseMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
