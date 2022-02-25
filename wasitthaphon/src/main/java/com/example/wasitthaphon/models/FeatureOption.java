c4f6d636d79907a74c7d75fb2637dbf443454ffb
wasitthaphon <wasitthaphon@gmail.com>
2022-02-25 21:37:52 +0700

    add: สร้าง FeatureOption entity #47

---
+++ wasitthaphon/src/main/java/com/example/wasitthaphon/models/FeatureOption.java
@@ -0,0 +1,44 @@
+package com.example.wasitthaphon.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "featureOption")
+public class FeatureOption {
+    @Id
+    private int featureOptionId;
+
+    @OneToOne
+    @JoinColumn(name = "feature_id")
+    private Feature feature;
+
+    private String option;
+
+    public int getFeatureOptionId() {
+        return featureOptionId;
+    }
+
+    public void setFeatureOptionId(int featureOptionId) {
+        this.featureOptionId = featureOptionId;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+}
