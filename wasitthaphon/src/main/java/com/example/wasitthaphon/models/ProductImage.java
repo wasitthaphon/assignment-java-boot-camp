dcc87312854bbba3626976d62f7cc6f167b4fe93
wasitthaphon <wasitthaphon@gmail.com>
2022-02-26 12:48:08 +0700

    add: สร้าง Entity ProductImage #62

---
+++ wasitthaphon/src/main/java/com/example/wasitthaphon/models/ProductImage.java
@@ -0,0 +1,47 @@
+package com.example.wasitthaphon.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "productImages")
+public class ProductImage {
+    @Id
+    private int id;
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "produc_id")
+    private Product product;
+
+    private String productImageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getProductImageUrl() {
+        return productImageUrl;
+    }
+
+    public void setProductImageUrl(String productImageUrl) {
+        this.productImageUrl = productImageUrl;
+    }
+}
