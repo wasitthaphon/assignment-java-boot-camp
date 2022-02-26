059da7835ec46af47045a42ee2b2e750b1bab165
wasitthaphon <wasitthaphon@gmail.com>
2022-02-26 13:20:19 +0700

    add: สร้าง FeatureOption repository #69

---
+++ wasitthaphon/src/main/java/com/example/wasitthaphon/repositories/FeatureOptionRepository.java
@@ -0,0 +1,10 @@
+package com.example.wasitthaphon.repositories;
+
+import com.example.wasitthaphon.models.FeatureOption;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FeatureOptionRepository extends JpaRepository<FeatureOption, Integer> {
+
+}
