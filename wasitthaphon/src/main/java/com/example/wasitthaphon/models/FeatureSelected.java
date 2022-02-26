3b46954a90ae2d71895bc85fb2c80b5165549b2a
wasitthaphon <wasitthaphon@gmail.com>
2022-02-26 19:45:43 +0700

    change: เพิ่มเติม FeatureSelected Entity #75

--- wasitthaphon/src/main/java/com/example/wasitthaphon/models/FeatureSelected.java
+++ wasitthaphon/src/main/java/com/example/wasitthaphon/models/FeatureSelected.java
@@ -28,5 +28,44 @@ public class FeatureSelected {
     @JoinColumn(name = "featureOption_id")
     private FeatureOption featureOption;
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
+
+    public FeatureOption getFeatureOption() {
+        return featureOption;
+    }
+
+    public void setFeatureOption(FeatureOption featureOption) {
+        this.featureOption = featureOption;
+    }
+
 }
